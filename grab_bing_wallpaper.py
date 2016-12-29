5dad7f19b815507e2c44b89baff57ce1e3ac1e00
dafengge0913 <dafengge0913@hotmail.com>
2016-12-29 10:51:47 +0800

    Fix image url

--- grab_bing_wallpaper.py
+++ grab_bing_wallpaper.py
@@ -11,16 +11,21 @@ Enter directly or input 0 for the today's: "))
 except ValueError:
     idx = 0
 
+base_url = "http://cn.bing.com"
+
 time_stamp = int(round(time.time() * 1000))
-url_pattern = "http://cn.bing.com/HPImageArchive.aspx?format=js&idx={idx}&n=1&nc={time_stamp}&pid=hp&video=0"
+url_pattern = base_url + "/HPImageArchive.aspx?format=js&idx={idx}&n=1&nc={time_stamp}&pid=hp&video=0"
 url = url_pattern.format(idx=idx, time_stamp=time_stamp)
 data = urlopen(url).read().decode('utf-8')
 json_str = json.loads(data)
+
 try:
     img_url = json_str["images"][0]["url"]
 except TypeError:
     print("can not found url, maybe it is too early")
     exit()
+
+img_url = base_url + img_url
 print("download", img_url)
 file_name = img_url.split("/")[-1]
 urlretrieve(img_url, r"C:\Users\wangjf\Desktop\other\壁纸\\" + file_name)
